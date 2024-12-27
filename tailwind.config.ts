a7b04a5d24f9aab754ee93094974badc7f62877a
Saidakbar <saidakbar.dev@gmail.com>
2024-12-27 18:21:54 +0500

    init

---
+++ tailwind.config.ts
@@ -0,0 +1,36 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        gilroy: ['Gilroy', 'sans-serif'],
+        roadRadio: ['RoadRadio', 'sans-serif'],
+      },
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        gray: {
+          DEFAULT: '#898989',
+        },
+        black: {
+          DEFAULT: '#222529',
+        },
+        orange: {
+          DEFAULT: '#FB7C0E',
+        },
+        lightGray: '#E5E5E5',
+        iconBackground: '#FFF0E8',
+        grayBorder: 'rgba(34, 37, 41, 0.10)',
+        storeLogoBg: 'rgba(34, 37, 41, 0.04)',
+        sliderBg: 'rgba(60, 60, 67, 0.18)'
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
