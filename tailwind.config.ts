e20ed9a42c27332830ef7216fa61eee77e9236ab
Saidakbar <saidakbar.dev@gmail.com>
2025-01-01 00:19:31 +0500

    icons

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -24,12 +24,16 @@ export default {
         },
         orange: {
           DEFAULT: '#FB7C0E',
+          '05': 'rgba(251, 124, 14, 0.05)'
         },
         lightGray: '#E5E5E5',
         iconBackground: '#FFF0E8',
         grayBorder: 'rgba(34, 37, 41, 0.10)',
         storeLogoBg: 'rgba(34, 37, 41, 0.04)',
         sliderBg: 'rgba(60, 60, 67, 0.18)'
+      },
+      borderRadius: {
+        '80': '80px',
       },
     },
   },
