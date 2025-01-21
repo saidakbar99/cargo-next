9eb4ae3dad5212540f272e7fe395290a8b708ee6
Saidakbar <saidakbar.dev@gmail.com>
2025-01-21 18:10:21 +0500

    added marker to map

--- src/app/components/MyMap.tsx
+++ src/app/components/MyMap.tsx
@@ -1,7 +1,7 @@
-import { GoogleMap, LoadScript } from '@react-google-maps/api';
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 
 const containerStyle = { width: '100%', height: '400px', borderRadius: '16px', };
-const center = { lat: 41.2995, lng: 69.2401 };
+const center = { lat: 41.3158, lng: 69.2789 };
 const mapOptions = {
   mapTypeControl: false,        // Remove map type (satellite, terrain, etc.)
   streetViewControl: false,     // Remove street view (human icon)
@@ -13,7 +13,7 @@ const MyMap = () => {
   return (
     <LoadScript googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || ''}>
       <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={15} options={mapOptions}>
-        {/* Add markers or other components here */}
+        <Marker position={center} />
       </GoogleMap>
     </LoadScript>
   );
