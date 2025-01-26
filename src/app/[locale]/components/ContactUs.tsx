46f299a5af856202f5232aa2cc8e1f0f4a69e3ea
JahongirEshmurodov <johonbek0808@gmail.com>
2025-01-26 22:46:01 +0500

    contactus + footer i 18next

--- src/app/[locale]/components/ContactUs.tsx
+++ src/app/[locale]/components/ContactUs.tsx
@@ -3,35 +3,37 @@
 import { IconWithBackground } from "./ui/IconWithBackground"
 import { Button } from "./ui/Button"
 import MyMap from "./MyMap"
+import { useTranslations } from "next-intl"
 
 export const ContactUs = () => {
+  const t = useTranslations();
   return (
     <div id="contactUs" className="mt-[18px] lg:mt-[108px]">
-      <h2 className="font-roadRadio font-bold text-[32px] lg:text-[50px] text-center mb-9 lg:mb-12">связаться с нами</h2>
-      <MyMap />
+      <h2 className="font-roadRadio font-bold text-[32px] lg:text-[50px] text-center mb-9 lg:mb-12">{t('contactUs')}</h2>
+      <MyMap /> 
       <div className="mt-9 lg:mt-12 flex flex-col lg:flex-row">
         <div className="p-6 lg:p-12 bg-black lg:min-w-[464px] order-2 lg:order-none rounded-xl lg:rounded-e-none">
           <div className="py-4 flex">
             <IconWithBackground icon='/svgs/mail_icon.svg' />
             <div className="flex flex-col ml-5">
-              <span className="mb-2 text-white text-xl font-semibold">Напишите нам</span>
-              <span className="text-lightGray font-medium">Наша почта:</span>
+              <span className="mb-2 text-white text-xl font-semibold">{t('contactUstitle')}</span>
+              <span className="text-lightGray font-medium">{t('contactUstext')}</span>
               <span className="text-orange font-medium">garantiexpresscargo@gmail.com</span>
             </div>
           </div>
           <div className="py-4 flex my-8">
             <IconWithBackground icon='/svgs/geo_pin_icon.svg' />
             <div className="flex flex-col ml-5">
-              <span className="mb-2 text-white text-xl font-semibold">Офис</span>
-              <span className="text-lightGray font-medium">Приходите наш офис</span>
+              <span className="mb-2 text-white text-xl font-semibold">{t('contactUstitle2')}</span>
+              <span className="text-lightGray font-medium">{t("contactUstext2")}</span>
               <span className="text-orange font-medium">Istanbul Kadıköy Ösmanağa cd cilek sok kuçuk sari is hani no 6 kat 2 dayre 10 </span>
             </div>
           </div>
           <div className="py-4 flex">
             <IconWithBackground icon='/svgs/phone_icon.svg' />
             <div className="flex flex-col ml-5">
-              <span className="mb-2 text-white text-xl font-semibold">Телефон</span>
-              <span className="text-lightGray font-medium">пн-сб с 9:00 до 21:00.</span>
+              <span className="mb-2 text-white text-xl font-semibold">{t('contactUstitle3')}</span>
+              <span className="text-lightGray font-medium">{t('contactUstext3')}</span>
 
               <table className="mt-[14px] text-lightGray font-medium">
                 <tbody>
@@ -81,18 +83,18 @@ export const ContactUs = () => {
         <div className="p-6 lg:p-24 w-full order-1 lg:order-none border border-lightGray rounded-xl lg:rounded-s-none mb-9 lg:mb-0">
           <div className="flex flex-col mb-6">
             <label htmlFor="name" className="text-sm mb-1.5 font-semibold">
-              Имя
+              {t('contactUsinp')}
             </label>
             <input
               id="name"
               type="text"
               className="border border-lightGray py-3 px-4 rounded-lg w-full"
-              placeholder="Ваше имя"
+              placeholder={t('contactUsinpplace')}
             />
           </div>
           <div className="flex flex-col mb-6">
             <label htmlFor="telephone" className="text-sm mb-1.5 font-semibold">
-              Номер телефона
+              {t('contactUsinp2')}
             </label>
             <input
               id="telephone"
@@ -103,7 +105,7 @@ export const ContactUs = () => {
           </div>
           <div className="flex flex-col mb-6">
             <label htmlFor="message" className="text-sm mb-1.5 font-semibold">
-              Сообщение
+              {t('contactUsinp3')}
             </label>
             <textarea
               id="message"
@@ -114,9 +116,9 @@ export const ContactUs = () => {
           </div>
           <div className="flex mb-8">
             <input type="checkbox" className=" w-5 h-5 cursor-pointer accent-orange rounded-md" />
-            <span className="text-gray ml-3">Вы соглашаетесь с политикой конфиденциальности.</span>
+            <span className="text-gray ml-3">{t('contactUscheck')}</span>
           </div>
-          <Button className="w-full" variant="orange" text="Отправить сообщение" />
+          <Button className="w-full" variant="orange" text={t('contactUsbutton')} />
         </div>
       </div>
     </div>
--- src/app/[locale]/components/Footer.tsx
+++ src/app/[locale]/components/Footer.tsx
@@ -1,64 +1,66 @@
+import { useTranslations } from "next-intl";
 import { SocialMedias } from "./ui/SocialMedias"
 
 export const Footer = () => {
+  const t = useTranslations();
   return (
     <footer className="bg-black mt-[50px] lg:mt-[140px] lg:pt-16 lg:pb-12">
       <div className="mx-auto xl:w-[1216px] p-4 lg:py-8">
         <div className="hidden lg:flex mb-8">
           <div className="w-[320px] mr-16">
             <img className='mb-6' src='/svgs/cargo_white.svg' alt="Logo" />
-            <span className='text-white font-medium'>Создавайте удивительные цифровые впечатления, которые делают мир более счастливым.</span>
+            <span className='text-white font-medium'>{t('footertitle')}</span>
           </div>
           <div>
             <div className="grid grid-cols-4 gap-8">
               <div>
-                <h2 className="mb-6 text-sm font-semibold text-white">Товар</h2>
+                <h2 className="mb-6 text-sm font-semibold text-white">{t('footerlist')}</h2>
                 <ul className="text-lightGray font-medium">
-                  <li className="mb-4">Обзор</li>
-                  <li className="mb-4">Функции</li>
-                  <li className="mb-4">Решения</li>
-                  <li className="mb-4">Учебники</li>
-                  <li className="mb-4">Цены</li>
-                  <li className="mb-4">Релизы</li>
+                  <li className="mb-4">{t('footerlink')}</li>
+                  <li className="mb-4">{t('footerlink2')}</li>
+                  <li className="mb-4">{t('footerlink3')}</li>
+                  <li className="mb-4">{t('footerlink4')}</li>
+                  <li className="mb-4">{t('footerlink5')}</li>
+                  <li className="mb-4">{t('footerlink6')}</li>
                 </ul>
               </div>
               <div>
-                  <h2 className="mb-6 text-sm font-semibold text-white">Ресурсы</h2>
+                  <h2 className="mb-6 text-sm font-semibold text-white">{t('footerlist2')}</h2>
                   <ul className="text-lightGray font-medium">
-                    <li className="mb-4">Блог</li>
-                    <li className="mb-4">Новостная</li>
-                    <li className="mb-4">События</li>
-                    <li className="mb-4">Справочный центр</li>
-                    <li className="mb-4">Учебники</li>
-                    <li className="mb-4">Поддерживать</li>
+                    <li className="mb-4">{t('footerlink7')}</li>
+                    <li className="mb-4">{t('footerlink8')}</li>
+                    <li className="mb-4">{t('footerlink9')}</li>
+                    <li className="mb-4">{t('footerlink10')}</li>
+                    <li className="mb-4">{t('footerlink11')}</li>
+                    <li className="mb-4">{t('footerlink12')}</li>
                   </ul>
               </div>
               <div>
-                  <h2 className="mb-6 text-sm font-semibold text-white">Компания</h2>
+                  <h2 className="mb-6 text-sm font-semibold text-white">{t('footerlist3')}</h2>
                   <ul className="text-lightGray font-medium">
-                    <li className="mb-4">О нас</li>
-                    <li className="mb-4">Карьера</li>
-                    <li className="mb-4">Press</li>
-                    <li className="mb-4">Новости</li>
-                    <li className="mb-4">Галерея</li>
-                    <li className="mb-4">Контакт</li>
+                    <li className="mb-4">{t('footerlink13')}</li>
+                    <li className="mb-4">{t('footerlink14')}</li>
+                    <li className="mb-4">{t('footerlink15')}</li>
+                    <li className="mb-4">{t('footerlink16')}</li>
+                    <li className="mb-4">{t('footerlink17')}</li>
+                    <li className="mb-4">{t('footerlink18')}</li>
                   </ul>
               </div>
               <div>
-                  <h2 className="mb-6 text-sm font-semibold text-white">Юридический</h2>
+                  <h2 className="mb-6 text-sm font-semibold text-white">{t('footerlist4')}</h2>
                   <ul className="text-lightGray font-medium">
-                    <li className="mb-4">Условия</li>
-                    <li className="mb-4">Конфиденциальность</li>
-                    <li className="mb-4">Cookies</li>
-                    <li className="mb-4">Лицензии</li>
-                    <li className="mb-4">Настройки</li>
+                    <li className="mb-4">{t('footerlink19')}</li>
+                    <li className="mb-4">{t('footerlink20')}</li>
+                    <li className="mb-4">{t('footerlink21')}</li>
+                    <li className="mb-4">{t('footerlink22')}</li>
+                    <li className="mb-4">{t('footerlink23')}</li>
                   </ul>
               </div>
             </div>
           </div>
         </div>
         <div className="w-full flex justify-between lg:pt-12">
-          <span className="text-white font-medium">© Все права защищены!</span>
+          <span className="text-white font-medium">{t("footer")}</span>
           <SocialMedias />
         </div>
       </div>
