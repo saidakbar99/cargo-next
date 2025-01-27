c5728c338f91b98d7a61b1e92b985841f2417618
Saidakbar <saidakbar.dev@gmail.com>
2025-01-28 00:06:21 +0500

    added social media links

--- src/app/[locale]/components/ui/SocialMedias.tsx
+++ src/app/[locale]/components/ui/SocialMedias.tsx
@@ -1,13 +1,13 @@
 export const SocialMedias = () => {
   return (
     <div className="flex items-center">
-      <a href="https://www.instagram.com/">
+      <a target="_blank" href="https://www.instagram.com/garanti_express_carco_/">
         <img width={24} height={24} src="/svgs/instagram.svg" alt="Instagram Logo" />
       </a>
-      <a href="https://www.facebook.com/">
+      <a target="_blank" href="https://www.facebook.com/profile.php?id=100011275953923">
         <img className="mx-6" width={24} height={24} src="/svgs/facebook.svg" alt="Facebook Logo" />
       </a>
-      <a href="https://t.me/telegram">
+      <a target="_blank" href="https://t.me/garantiexpresskargo">
         <img width={24} height={24} src="/svgs/telegram.svg" alt="Telegram Logo" />
       </a>
     </div>
