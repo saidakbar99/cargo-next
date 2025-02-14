4394eb9e8faff69770811944187e80cbd22e7dd3
JahongirEshmurodov <johonbek0808@gmail.com>
2025-02-14 14:51:39 +0500

    .

--- src/app/[locale]/components/ui/SocialButton.tsx
+++ src/app/[locale]/components/ui/SocialButton.tsx
@@ -1,3 +1,5 @@
+import { useTranslations } from "next-intl";
+
 interface ButtonProps {
   handleClick?: () => void;
   classname?: string;
@@ -7,6 +9,7 @@ export const SocialButton: React.FC<ButtonProps> = ({
   handleClick,
   classname = '',
 }) => {
+   const t = useTranslations();
   return (
     <button
       onClick={handleClick}
@@ -14,7 +17,7 @@ export const SocialButton: React.FC<ButtonProps> = ({
         flex justify-center px-5 py-2.5 mt-2 ${classname}`}
     >
       <img src='/svgs/google_icon.svg' width={20} height={20} alt='Google Icon' />
-      <span className='ml-2'>Google orqali kirish</span>
+      <span className='ml-2'>{t("socialButton")}</span>
     </button>
   )
 }
