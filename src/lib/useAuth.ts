5d4fcddd5c490262245c7bb9d36e29eb522f5c5d
Muhammadrahimw <muqalla21@gmail.com>
2025-02-21 18:04:47 +0500

    fixed localStorage bug of useAuth

--- src/lib/useAuth.ts
+++ src/lib/useAuth.ts
@@ -1,37 +1,44 @@
-import axios from 'axios';
-import { useState, useEffect } from 'react';
+import axios from "axios";
+import {useState, useEffect} from "react";
 
 export function useAuth() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [isLoading, setIsLoading] = useState(true);
-  const [isProfileFilled, setIsProfileFilled] = useState(false);
-  const token = localStorage.getItem('access_token');
+	const [isAuthenticated, setIsAuthenticated] = useState(false);
+	const [isLoading, setIsLoading] = useState(true);
+	const [isProfileFilled, setIsProfileFilled] = useState(false);
+	const [token, setToken] = useState<string | null>(null);
+	useEffect(() => {
+		setToken(localStorage.getItem("access_token"));
+	}, []);
 
-  const fetchProfile = async () => {
-    try {
-      const response = await axios.get(
-        `${process.env.NEXT_PUBLIC_API_BASE_URL}customer/profile`,
-        {
-          headers: {
-            "Authorization": `Bearer ${token}`,
-          },
-        }
-      );
+	const fetchProfile = async () => {
+		try {
+			const response = await axios.get(
+				`${process.env.NEXT_PUBLIC_API_BASE_URL}customer/profile`,
+				{
+					headers: {
+						Authorization: `Bearer ${token}`,
+					},
+				}
+			);
 
-      if (response.data.full_name && response.data.passport && response.data.birth_date) {
-        setIsProfileFilled(true);
-      }
+			if (
+				response.data.full_name &&
+				response.data.passport &&
+				response.data.birth_date
+			) {
+				setIsProfileFilled(true);
+			}
 
-      setIsAuthenticated(!!token);
-      setIsLoading(false);
-    } catch (error) {
-      console.log(error)
-    }
-  }
+			setIsAuthenticated(!!token);
+			setIsLoading(false);
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
-  useEffect(() => {
-    fetchProfile();
-  }, []);
+	useEffect(() => {
+		fetchProfile();
+	}, []);
 
-  return { isAuthenticated, isLoading, isProfileFilled };
+	return {isAuthenticated, isLoading, isProfileFilled};
 }
